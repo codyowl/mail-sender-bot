a717b2bcc74f097f6888552e70837d9d9840f788
codyowl <codyowl@gmail.com>
2016-11-12 23:58:25 +0530

    new function added to get email bots number

--- mail_sender.py
+++ mail_sender.py
@@ -1,11 +1,14 @@
 import smtplib
 
 USER_NAME = "your username" 
+
 PASSWORD = "your password"
 
 SUBJECT = raw_input("Enter the subject :")
 
 CONTENT = raw_input("Enter the mail content :")
+
+EMAIL_BOT_COUNT = raw_input("How many email bots you want to create: ")
 
 FROM = "from@gmail.com"
 
@@ -13,8 +16,13 @@ TO = "to@live.com"
 
 Cc = ["ccs@gmail.com"]
 
-#TIME = int(raw_input("Enter your time in minutes to shutdown :"))
-
+def email_bot_creator(email_bot_count):
+    for i in range(len(email_bot_count)):
+        print "Enter details for bot no: %s" % (i)
+        to = raw_input("To: ")
+        subject = raw_input("Subject: ")
+        content = raw_input("enter your content: ")
+        time = raw_input("At what time you want to shoot this mail(mention am and pm): ")
 
 message = "From: %s\r\n" % FROM + "To: %s\r\n" % TO + "CC: %s\r\n" % ",".join(Cc)+ "Subject: %s\r\n" % SUBJECT + "\r\n" + CONTENT
 
