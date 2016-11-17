34dd88cf153b2733e7df5e906f33156af7005739
codyowl <codyowl@gmail.com>
2016-11-17 18:12:05 +0530

    cc checker added, time raw input added :heavy_plus_sign:

--- mail_sender.py
+++ mail_sender.py
@@ -4,10 +4,6 @@ USER_NAME = "your username"
 
 PASSWORD = "your password"
 
-SUBJECT = raw_input("Enter the subject :")
-
-CONTENT = raw_input("Enter the mail content :")
-
 EMAIL_BOT_COUNT = raw_input("How many email bots you want to create: ")
 
 FROM = "from@gmail.com"
@@ -16,17 +12,21 @@ TO = "to@live.com"
 
 Cc = ["ccs@gmail.com"]
 
+
 def email_bot_creator(email_bot_count):
     for i in range(len(email_bot_count)):
-        print "Enter details for bot no: %s" % (i)
-        to = raw_input("To: ")
-        subject = raw_input("Subject: ")
-        content = raw_input("enter your content: ")
-        time = raw_input("At what time you want to shoot this mail(mention am and pm): ")
+        print "Enter details for bot no: %s" % (i + 1)
+        to = raw_input("Enter the To address: ")
+        cc_checker = raw_input("Do you want to add cc ?(y/n): ")
+        if cc_checker.lower() == 'y':
+        	cc = raw_input("Enter the Cc address: ")
+        subject = raw_input("Enter the Subject: ")
+        content = raw_input("Enter the content: ")
+        time = raw_input("At what time you want to shoot this mail(mention like this hh:mm with am and pm): ")
 
-message = "From: %s\r\n" % FROM + "To: %s\r\n" % TO + "CC: %s\r\n" % ",".join(Cc)+ "Subject: %s\r\n" % SUBJECT + "\r\n" + CONTENT
+# message = "From: %s\r\n" % FROM + "To: %s\r\n" % TO + "CC: %s\r\n" % ",".join(Cc)+ "Subject: %s\r\n" % SUBJECT + "\r\n" + CONTENT
 
-receivers = [TO] + Cc        
+# receivers = [TO] + Cc        
 
 def mail_sender(username, password, FROM, receivers, message):
 	# SECONDS = TIME * 60
@@ -45,5 +45,5 @@ def mail_sender(username, password, FROM, receivers, message):
 	   print "failed to send the mail" 
 	
 
-
-mail_sender(USER_NAME, PASSWORD, FROM, receivers, message)
+email_bot_creator(EMAIL_BOT_COUNT)
+# mail_sender(USER_NAME, PASSWORD, FROM, receivers, message)
