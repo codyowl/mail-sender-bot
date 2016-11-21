aae0389c282b47bb4f6a67abae27070cb5cc70e5
codyowl <codyowl@gmail.com>
2016-11-21 18:43:45 +0530

    railway time converter and time processor added :heavy_plus_sign:

--- mail_sender.py
+++ mail_sender.py
@@ -19,31 +19,55 @@ def email_bot_creator(email_bot_count):
         to = raw_input("Enter the To address: ")
         cc_checker = raw_input("Do you want to add cc ?(y/n): ")
         if cc_checker.lower() == 'y':
-        	cc = raw_input("Enter the Cc address: ")
+            cc = raw_input("Enter the Cc address: ")
         subject = raw_input("Enter the Subject: ")
         content = raw_input("Enter the content: ")
         time = raw_input("At what time you want to shoot this mail(mention like this hh:mm with am and pm): ")
+        time_processor_method = time_processor(time)
+        hours = time_processor_method[0]
+        minutes = time_processor_method[1]
+        meridiem = time_processor_method[2]
+        railway_time_converter_method = railway_time_converter(hours, minutes, meridiem)
+        print railway_time_converter_method
+        
+def railway_time_converter(hours, minutes, meridiem):
+    if meridiem == 'pm':
+        hours = int(hours) + 12
+        time = '%s:%s' % (hours, minutes)
+    else:
+        time = '%s:%s' % (hours, time)
+    return time  
+    
+def time_processor(time):
+    time_splitter = time.split(':')
+    meridiem_splitter = time_splitter[1].split(' ')
+    hours = time_splitter[0]
+    minutes = meridiem_splitter[0]
+    meridiem = meridiem_splitter[1]
+    return hours, minutes, meridiem
+
+
 
 # message = "From: %s\r\n" % FROM + "To: %s\r\n" % TO + "CC: %s\r\n" % ",".join(Cc)+ "Subject: %s\r\n" % SUBJECT + "\r\n" + CONTENT
 
 # receivers = [TO] + Cc        
 
 def mail_sender(username, password, FROM, receivers, message):
-	# SECONDS = TIME * 60
+    # SECONDS = TIME * 60
     # time.sleep(float(SECONDS))
 
-	try:
-		server = smtplib.SMTP("smtp.gmail.com", 587)
-		server.ehlo()
-		server.starttls()
-		server.login(username, password)
-		server.sendmail(FROM, receivers, message)
-		server.close()
-		print "mail sent !"
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(FROM, receivers, message)
+        server.close()
+        print "mail sent !"
 
-	except:
-	   print "failed to send the mail" 
-	
+    except:
+       print "failed to send the mail" 
+    
 
 email_bot_creator(EMAIL_BOT_COUNT)
 # mail_sender(USER_NAME, PASSWORD, FROM, receivers, message)
